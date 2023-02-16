fc0b72b97730fdadbc1f936a1efadc9dc2335ca3
Rayane Serir <serir_r@etna-alternance.net>
2023-02-16 11:42:58 +0100

    v2 beta

--- src/index.ts
+++ src/index.ts
@@ -31,13 +31,9 @@ import { attributeService } from 'spinal-env-viewer-plugin-documentation-service
 import { 
     spinalAnalyticService,
     IAnalytic,
-    IAnalysisProcess,
-    IEntityType,
-    ITrackedVariableMethod,
-    Analytic,
-    AnalysisProcess,
-    EntityType,
-    TrackedVariableMethod,
+    IEntity,
+    IConfig,
+    ITrackingMethod,
     ENTITY_TYPES,
     ALGORITHMS,
     ANALYTIC_RESULT_TYPE,
@@ -81,52 +77,138 @@ class SpinalMain {
 
     public async createTestEnvironment() {
         const context = await spinalAnalyticService.createContext("testContext");
-        const IEntityInfo : IEntityType = {
+        const IEntityInfo : IEntity = {
             name: "Pièces",
             standard_name: "Rooms",
             entityType: ENTITY_TYPES.ROOM,
             description:""
         };
-        const IAnalysisProcess: IAnalysisProcess = {
+        const IAnalytic: IAnalytic = {
             name: "Température anormale",
-            intervalTime: 0,
             description :""
         };
 
-        const IAnalytic : IAnalytic = {
-            name: ALGORITHMS.THRESHOLD_BELOW,
+        const IConfig : IConfig = {
+            algorithm: ALGORITHMS.THRESHOLD_ABOVE,
             resultType: ANALYTIC_RESULT_TYPE.TICKET,
             resultName:"Température anormale",
-            description: "",
+            intervalTime: 0
 
         };
-        const ITrackedVariableMethod : ITrackedVariableMethod = {
-            name: "TrackMethod",
+        const algorithmParameters = [
+            { name: "p1" , value: 15 , type: "number"},
+        ];
+        const ITrackingMethod : ITrackingMethod = {
+            name: "TrackingMethod",
             trackMethod: TRACK_METHOD.CONTROL_ENDPOINT_NAME_FILTER,
             filterValue: "Température"
         }
 
         const entityInfo = await spinalAnalyticService.addEntity(IEntityInfo,context.id.get());
-        const analysisProcessInfo = await spinalAnalyticService.addAnalysisProcess(IAnalysisProcess,context.id.get(),entityInfo.id.get());
-        const analyticInfo = await spinalAnalyticService.addAnalytic(IAnalytic,context.id.get(),analysisProcessInfo.id.get());
-        const trackedVariableMethod = await spinalAnalyticService.addTrackedVariableMethod(ITrackedVariableMethod,context.id.get(),analysisProcessInfo.id.get());
-        const followedEntity = await spinalAnalyticService.addLinkToFollowedEntity(context.id.get(),
-        analysisProcessInfo.id.get(),"SpinalNode-69e8c850-4d04-ace6-7db2-3e3bf4e4d944-1825e3c67cc");
+        const analyticInfo = await spinalAnalyticService.addAnalytic(IAnalytic,context.id.get(),entityInfo.id.get());
+        const trackingMethodInfo = await spinalAnalyticService.addInputTrackingMethod(ITrackingMethod,context.id.get(),analyticInfo.id.get());
+        const configInfo = await spinalAnalyticService.addConfig(IConfig,algorithmParameters,analyticInfo.id.get(),context.id.get());
+        /*const followedEntityInfo = await spinalAnalyticService.addInputLinkToFollowedEntity(context.id.get(),
+        analyticInfo.id.get(),"SpinalNode-69e8c850-4d04-ace6-7db2-3e3bf4e4d944-1825e3c67cc");
+        */
+
+        // groupe de salles ou étage
+        const followedEntityInfo = await spinalAnalyticService.addInputLinkToFollowedEntity(context.id.get(),
+        analyticInfo.id.get(),"SpinalNode-277d9c8d-efa0-8cba-7fa5-3632cb307207-1825e3c98c3");
+
+
     }
 
+    public async createTestEnvironment2() {
+        const context = await spinalAnalyticService.createContext("testContext");
+        const IEntityInfo : IEntity = {
+            name: "Pièces",
+            standard_name: "Rooms",
+            entityType: ENTITY_TYPES.ROOM,
+            description:""
+        };
+        const IAnalytic: IAnalytic = {
+            name: "Régulation de la température",
+            description :""
+        };
+
+        const IConfig : IConfig = {
+            algorithm: ALGORITHMS.PUTVALUE,
+            resultType: ANALYTIC_RESULT_TYPE.MODIFY_CONTROL_ENDPOINT,
+            resultName:"Régulation de la température",
+            intervalTime: 0
+
+        };
+        const algorithmParameters = [
+            { name: "p1" , value: 22 , type: "number"},
+        ];
+        const ITrackingMethod : ITrackingMethod = {
+            name: "TrackingMethod",
+            trackMethod: TRACK_METHOD.CONTROL_ENDPOINT_NAME_FILTER,
+            filterValue: "Température"
+        }
+
+        const entityInfo = await spinalAnalyticService.addEntity(IEntityInfo,context.id.get());
+        const analyticInfo = await spinalAnalyticService.addAnalytic(IAnalytic,context.id.get(),entityInfo.id.get());
+        const trackingMethodInfo = await spinalAnalyticService.addInputTrackingMethod(ITrackingMethod,context.id.get(),analyticInfo.id.get());
+        const configInfo = await spinalAnalyticService.addConfig(IConfig,algorithmParameters,analyticInfo.id.get(),context.id.get());
+        /*const followedEntityInfo = await spinalAnalyticService.addInputLinkToFollowedEntity(context.id.get(),
+        analyticInfo.id.get(),"SpinalNode-69e8c850-4d04-ace6-7db2-3e3bf4e4d944-1825e3c67cc");
+        */
+
+        // groupe de salles ou étage
+        const followedEntityInfo = await spinalAnalyticService.addInputLinkToFollowedEntity(context.id.get(),
+        analyticInfo.id.get(),"SpinalNode-277d9c8d-efa0-8cba-7fa5-3632cb307207-1825e3c98c3");
+
+    }
 
     public async initJob(){
         const contexts = spinalAnalyticService.getContexts();
         for (const context of contexts){
-            const analysisProcesses = await spinalAnalyticService.getAllAnalysisProcesses(context.id.get());
-            for (const analysisProcess of analysisProcesses){
-                const entryDataModels = await spinalAnalyticService.applyTrackedVariableMethod(analysisProcess.id.get());
+            const analytics = await spinalAnalyticService.getAllAnalytics(context.id.get());
+            for (const analytic of analytics){
+                // si intervalTime = 0 => method COV
+                const config = await spinalAnalyticService.getConfig(analytic.id.get());
+                console.log(config.intervalTime.get());
+                if (config.intervalTime.get() === 0){
+                    const entities = await spinalAnalyticService.getWorkingFollowedEntities(analytic.id.get());
+                    for (const entity of entities){
+                        const entryDataModels = await spinalAnalyticService.getEntryDataModelsFromFollowedEntity(analytic.id.get(),entity);
+                        for (const entryDataModel of entryDataModels){
+                            const valueModel = (await entryDataModel.element.load());
+                            valueModel.bind(
+                                () => {
+                                    console.log("Value changed, starting analysis...");
+                                    spinalAnalyticService.doAnalysis(analytic.id.get(),entity);
+                                },false
+                            )
+                        }
+                    }
+                }
+                else {
+                    const entities = await spinalAnalyticService.getWorkingFollowedEntities(analytic.id.get());
+                    for (const entity of entities){
+                        setInterval(() => {
+                            spinalAnalyticService.doAnalysis(analytic.id.get(),entity);
+                        },config.intervalTime.get())
+                    }
+                }
+            }
+        }
+    }
+
+    /*public async initJob(){
+        const contexts = spinalAnalyticService.getContexts();
+        for (const context of contexts){
+            const analytics = await spinalAnalyticService.getAllAnalytics(context.id.get());
+            for (const analytic of analytics){
+                const entryDataModels = await spinalAnalyticService.applyTrackingMethodLegacy(analytic.id.get());
                 for (const entryDataModel of entryDataModels){
                     const valueModel = (await entryDataModel.element.load());
                     valueModel.bind(
                         () => {
                             console.log("Value changed, starting analysis...");
-                            spinalAnalyticService.doAnalysis(analysisProcess.id.get());
+                            spinalAnalyticService.doAnalysis(analytic.id.get());
                         },false
                     )
                     
@@ -136,7 +218,7 @@ class SpinalMain {
             }
         }
 
-    }
+    }*/
 
     /*public async initJob(){
         // console.log(serviceAnalysis);
@@ -284,7 +366,7 @@ async function Main(){
     const spinalMain = new SpinalMain();
     await spinalMain.init();
     await spinalMain.initContext();
-    //await spinalMain.createTestEnvironment();
+    //await spinalMain.createTestEnvironment2();
     await spinalMain.initJob();
     /*
     // await spinalMain.job();
